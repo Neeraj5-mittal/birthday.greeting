4d3eda7b4b0cdbd415beb23f62447f93b3681f71
Neeraj5-mittal <neerajmittal2004@gmail.com>
2024-10-02 15:15:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Show a birthday message after a few seconds
+setTimeout(() => {
+    alert("Happy Birthday, Dad! You’ve always been my greatest role model and supporter. Wishing you a year filled with happiness, love, good health, and unforgettable memories. Thanks for everything you do!");
+}, 5000);
